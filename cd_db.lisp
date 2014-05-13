f08e96a18e53ce8db5a7b5238ae69118e30716cb
Jeremy Song <jeremy.song@me.com>
2014-05-13 05:09:03 -0400

    Add select functions

--- cd_db.lisp
+++ cd_db.lisp
@@ -1,4 +1,4 @@
-;;;(defvar *db* nil)
+(defvar *db* nil)
 
 (defun make-cd (title artist rating ripped)
   (list :title title :artist artist :rating rating :ripped ripped))
@@ -9,3 +9,53 @@
 (defun dump-db ()
   (dolist (cd *db*)
     (format t "~{~a:~10t~a~%~}~%" cd)))
+
+
+(defun prompt-read (prompt)
+  (format *query-io* "~a: " prompt)
+  (force-output *query-io*)
+  (read-line *query-io*))
+
+
+(defun prompt-for-cd ()
+  (make-cd
+    (prompt-read "Title")
+    (prompt-read "Artist")
+    (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+    (y-or-n-p "Ripped [y/n]: ")))
+
+
+(defun add-cds ()
+  (loop (add-record (prompt-for-cd))
+      (if (not (y-or-n-p "Another? [y/n]: ")) (return))))
+
+
+(defun save-db (filename)
+  (with-open-file (out filename
+                   :direction :output
+                   :if-exists :supersede)
+    (with-standard-io-syntax (print *db* out))))
+
+
+(defun load-db (filename)
+  (with-open-file (in filename)
+    (with-standard-io-syntax (setf *db* (read in)))))
+
+
+(defun select-by-artist (artist)
+  (remove-if-not #'(lambda (cd) (equal (getf cd :artist) artist)) *db*))
+
+(defun select (selector-fn)
+  (remove-if-not selector-fn *db*))
+
+;;; This function returns a anonymous selector function
+(defun artist-selector (artist)
+  #'(lambda (cd) (equal (getf cd :artist) artist)))
+
+(defun where (&key title artist rating (ripped nil ripped-p))
+  #'(lambda (cd)
+      (and
+        (if title (equal (getf cd :title) title) t)
+        (if artist (equal (getf cd :artist) artist) t)
+        (if rating (equal (getf cd :rating) rating) t)
+        (if ripped-p (equal (getf cd :ripped) ripped) t))))
